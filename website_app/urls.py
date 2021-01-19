5575fabe3bb85372031a869d0ec1ee4b392ff832
Dalius Deveikis <slkyper@gmail.com>
2021-01-19 21:14:25 +0200

    Add new app 'Account'; make some changes with bootstrap; add register & login templates; take navbar and footer from bootstrap; create base.html; correct some errors;

--- website_app/urls.py
+++ website_app/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from website_app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
+    path('', index, name='home'),
     path('', include('accounts.urls')),
 ]
--- website_app/views.py
+++ website_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import datetime
 
 def index(request):
     now = datetime.datetime.now()
     year = now.strftime("%Y")
     title_name = 'Python & Django'
-    return render(request, 'pages/base.html',{
+    return render(request, 'pages/index.html',{
         'date': now,
         'year': year,
         'title_name': title_name,
